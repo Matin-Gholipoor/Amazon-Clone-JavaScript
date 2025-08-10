bf8bd06aef85bad0105ea1d6a50a26e79cb63fc3
Matin-Gholipoor <matin.3108@gmail.com>
2025-08-10 17:16:39 +0330

    items quantity bug in checkout header fixed

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -2,12 +2,13 @@ import {
   Cart
 } from "../data/cart.js";
 
-const cart = new Cart('cart');
-
 export function showCheckoutHeader() {
+  const cart = new Cart('cart');
+  
   let itemsQuantity = 0;
   cart.getCartItems().forEach((item) => {
     itemsQuantity += item.quantity;
   });
+
   document.querySelector('.js-return-to-home-link').innerHTML = `${itemsQuantity} items`;
 }
